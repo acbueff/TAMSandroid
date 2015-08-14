8ce3dbbc592262b3cb723fa4191dfba592496967
Nati Tessema <nati.tessema@gmail.com>
2015-08-13 23:25:16 -0700

    Working on ListView.

--- TAMS2/app/src/main/java/com/dynamiclogic/tams/activity/MainActivity.java
+++ TAMS2/app/src/main/java/com/dynamiclogic/tams/activity/MainActivity.java
@@ -5,31 +5,43 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.dynamiclogic.tams.R;
+import com.dynamiclogic.tams.activity.fragment.PanelFragment;
+import com.dynamiclogic.tams.utils.SlidingUpPanelLayout;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 
 
-public class MainActivity extends Activity implements OnMapReadyCallback {
-
+public class MainActivity extends Activity implements OnMapReadyCallback,
+                                                        SlidingUpPanelLayout.PanelSlideListener,
+                                                        PanelFragment.OnFragmentInteractionListener {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate()");
         setContentView(R.layout.activity_main);
+
+        ((SlidingUpPanelLayout)getWindow().getDecorView().findViewById(R.id.sliding_layout)).setPanelSlideListener(this);
 
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume()");
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        Log.d(TAG, "onMapReady");
+        Log.d(TAG, "onMapReady()");
     }
 
     @Override
@@ -52,5 +64,40 @@ public class MainActivity extends Activity implements OnMapReadyCallback {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction() {
+        Log.d(TAG, "onFragmentInteraction()");
+    }
+
+    @Override
+    public void onPanelSlide(View panel, float slideOffset) {
+    }
+
+    @Override
+    public void onPanelCollapsed(View panel) {
+        Log.d(TAG, "onPanelCollapsed()");
+    }
+
+    @Override
+    public void onPanelExpanded(View panel) {
+        Log.d(TAG, "onPanelExpanded()");
+    }
+
+    @Override
+    public void onPanelAnchored(View panel) {
+        Log.d(TAG, "onPanelAnchored()");
+    }
+
+    @Override
+    public void onPanelHidden(View panel) {
+        Log.d(TAG, "onPanelHidden()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ((SlidingUpPanelLayout)getWindow().getDecorView().findViewById(R.id.sliding_layout)).setPanelSlideListener(null);
     }
 }
---
+++ TAMS2/app/src/main/java/com/dynamiclogic/tams/activity/fragment/PanelFragment.java
@@ -0,0 +1,100 @@
+package com.dynamiclogic.tams.activity.fragment;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import com.dynamiclogic.tams.R;
+
+public class PanelFragment extends Fragment {
+
+    private static final String TAG = MyAdapter.class.getSimpleName();
+
+    private OnFragmentInteractionListener mListener;
+    private ListView mListView;
+
+    public PanelFragment() { }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_panel, container, false);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction();
+    }
+
+    private class MyAdapter extends BaseAdapter {
+
+        private final String TAG = MyAdapter.class.getSimpleName();
+
+        public MyAdapter() {
+            Log.d(TAG, "MyAdapter()");
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Log.d(TAG, "getView()");
+            return null;
+        }
+    }
+
+}
